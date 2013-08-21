ba0202410688a575103abcf6631023cfbfb2c00e
Steve Smith <steve@kentsmiths.com>
2013-08-21 14:12:32 -0700

    Renaming the test

---
+++ src/GildedRoseKata/Tests/GildedRoseUpdateQualityShould.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace GildedRoseKata.Tests
+{
+    [TestFixture]
+    public class GildedRoseUpdateQualityShould
+    {
+        [Test]
+        public void ReduceQualityOfANormalItemByOneIfSellInIs10()
+        {
+            var items = new List<Item>();
+            items.Add(new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 });
+
+            var gildedrose = new GildedRose(items);
+            gildedrose.UpdateQuality();
+
+            var firstItem = items.First();
+
+            firstItem.Quality.Should().Be(19);
+            firstItem.SellIn.Should().Be(9);
+
+        }
+    }
+}
