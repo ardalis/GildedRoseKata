e8765ad05dda4f75c86ec189c828d7b47c797450
Steve Smith <steve@kentsmiths.com>
2013-08-21 16:04:07 -0700

    added nunit and FluentAssertions NuGet Packages
    And a first test that uses them.

---
+++ src/GildedRoseKata/Tests/QualityServiceShould.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace GildedRoseKata.Tests
+{
+    [TestFixture]
+    public class QualityServiceShould
+    {
+        [Test]
+        public void Pass()
+        {
+            var item = new Item();
+
+            item.Should().NotBeNull();
+        }
+    }
+}
