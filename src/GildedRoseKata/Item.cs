1eb9744c4b6988e29262cec8b80e4fd5abd3b959
Steve Smith <steve@kentsmiths.com>
2013-08-21 15:59:53 -0700

    Initial setup

---
+++ src/GildedRoseKata/Item.cs
@@ -0,0 +1,11 @@
+namespace GildedRoseKata
+{
+    public class Item
+    {
+        public string Name { get; set; }
+
+        public int SellIn { get; set; }
+
+        public int Quality { get; set; }
+    }
+}
