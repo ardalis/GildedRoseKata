3aa793dacdf82e3cfad979f8f8d939eb1ca3994a
Steve Smith <steve@kentsmiths.com>
2013-08-22 11:33:46 -0700

    Moved UpdateQuality() to QualityItem
    GildedRose is now only 23 lines long; all logic is in QualityItem now.

--- src/GildedRoseKata/GildedRose.cs
+++ src/GildedRoseKata/GildedRose.cs
@@ -2,50 +2,6 @@ using System.Collections.Generic;
 
 namespace GildedRoseKata
 {
-    class QualityItem
-    {
-        private readonly Item _item;
-
-        public QualityItem(Item item)
-        {
-            this._item = item;
-        }
-
-        public string Name
-        {
-            get
-            {
-                return _item.Name;
-            }
-            set
-            {
-                _item.Name = value;
-            }
-        }
-        public int Quality
-        {
-            get
-            {
-                return _item.Quality;
-            }
-            set
-            {
-                _item.Quality = value;
-            }
-        }
-        public int SellIn
-        {
-            get
-            {
-                return _item.SellIn;
-            }
-            set
-            {
-                _item.SellIn = value;
-            }
-        }
-
-    }
     class GildedRose
     {
         IList<Item> Items;
@@ -59,80 +15,8 @@ namespace GildedRoseKata
         {
             foreach (var item in Items)
             {
-                UpdateQuality(new QualityItem(item));
-            }
-        }
-
-        public void UpdateQuality(QualityItem item)
-        {
-            if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
-            {
-                if (item.Quality > 0)
-                {
-                    if (item.Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        item.Quality = item.Quality - 1;
-                    }
-                }
-            }
-            else
-            {
-                if (item.Quality < 50)
-                {
-                    item.Quality = item.Quality + 1;
-
-                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (item.SellIn < 11)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
-                        }
-
-                        if (item.SellIn < 6)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (item.Name != "Sulfuras, Hand of Ragnaros")
-            {
-                item.SellIn = item.SellIn - 1;
-            }
-
-            if (item.SellIn < 0)
-            {
-                if (item.Name != "Aged Brie")
-                {
-                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (item.Quality > 0)
-                        {
-                            if (item.Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                item.Quality = item.Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        item.Quality = item.Quality - item.Quality;
-                    }
-                }
-                else
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-                    }
-                }
+                var qualityItem = new QualityItem(item);
+                qualityItem.UpdateQuality();
             }
         }
     }
---
+++ src/GildedRoseKata/QualityItem.cs
@@ -0,0 +1,121 @@
+namespace GildedRoseKata
+{
+    class QualityItem
+    {
+        private readonly Item _item;
+
+        public QualityItem(Item item)
+        {
+            this._item = item;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _item.Name;
+            }
+            set
+            {
+                _item.Name = value;
+            }
+        }
+
+        public int Quality
+        {
+            get
+            {
+                return _item.Quality;
+            }
+            set
+            {
+                _item.Quality = value;
+            }
+        }
+
+        public int SellIn
+        {
+            get
+            {
+                return _item.SellIn;
+            }
+            set
+            {
+                _item.SellIn = value;
+            }
+        }
+
+        public void UpdateQuality()
+        {
+            if (this.Name != "Aged Brie" && this.Name != "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (this.Quality > 0)
+                {
+                    if (this.Name != "Sulfuras, Hand of Ragnaros")
+                    {
+                        this.Quality = this.Quality - 1;
+                    }
+                }
+            }
+            else
+            {
+                if (this.Quality < 50)
+                {
+                    this.Quality = this.Quality + 1;
+
+                    if (this.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (this.SellIn < 11)
+                        {
+                            if (this.Quality < 50)
+                            {
+                                this.Quality = this.Quality + 1;
+                            }
+                        }
+
+                        if (this.SellIn < 6)
+                        {
+                            if (this.Quality < 50)
+                            {
+                                this.Quality = this.Quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (this.Name != "Sulfuras, Hand of Ragnaros")
+            {
+                this.SellIn = this.SellIn - 1;
+            }
+
+            if (this.SellIn < 0)
+            {
+                if (this.Name != "Aged Brie")
+                {
+                    if (this.Name != "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (this.Quality > 0)
+                        {
+                            if (this.Name != "Sulfuras, Hand of Ragnaros")
+                            {
+                                this.Quality = this.Quality - 1;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        this.Quality = this.Quality - this.Quality;
+                    }
+                }
+                else
+                {
+                    if (this.Quality < 50)
+                    {
+                        this.Quality = this.Quality + 1;
+                    }
+                }
+            }
+        }
+    }
+}
